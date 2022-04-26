d15814a88597606bb875bfc71bfa513a866b7ef3
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-26 22:02:19 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -57,7 +57,7 @@ sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "$FIN_PATH"
 
 
 # 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明）
-# 当前可用机型 a311d, s922x, s905x3, s905x2, s905l3a, s912, s905d, s905x, s905w, s905
+# 当前可用机型 s922x s922x-n2 s922x-reva a311d s905x3 s905x2 s905x2-km3 s905l3a s912 s912-m8s s905d s905d-ki s905x s905w s905
 
 amlogic_model="s905d"
 amlogic_kernel="5.15.25"
