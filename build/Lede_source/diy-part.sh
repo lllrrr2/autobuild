38a034ccb826c5b7c9da1330eb583433cc3fb2c1
281677160 <66651997+281677160@users.noreply.github.com>
2022-07-26 14:13:41 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -63,7 +63,7 @@ sed -i "/exit 0/i\sed -i '/coremark/d' /etc/crontabs/root" "$FIN_PATH"
 #sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 
 
-# 更改使用OpenClash的分支代码，把下面的master改成dve就改成dve分支，改master就是用master分支
+# 更改使用OpenClash的分支代码，把下面的master改成dev就改成dev分支，改master就是用master分支
 echo "OpenClash_branch" > master
 
 
